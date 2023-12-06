6bc559c5cb5a413fcb70f0bcbf604b66bb3e320b
Maicoln <maicolnp@gmail.com.github.com>
2023-12-06 18:11:47 -0300

    Branch Tabela Usuario

---
+++ src/main/java/com/umbu/api/apiumbu/ApiumbuApplication.java
@@ -0,0 +1,13 @@
+package com.umbu.api.apiumbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiumbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiumbuApplication.class, args);
+	}
+
+}
