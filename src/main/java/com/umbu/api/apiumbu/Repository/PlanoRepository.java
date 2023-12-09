b0d506bc3017fdc9c274aa393940b7468cfa448f
Gabriel Borges <borgesgabriel205@.com>
2023-12-08 23:05:53 -0300

    correção das tabelas umchat e plano

--- src/main/java/com/umbu/api/apiumbu/Repository/PlanoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umbu.api.apiumbu.Repository;
-
-import com.umbu.api.apiumbu.Model.Plano;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import  java.util.UUID;
-
-public interface PlanoRepository extends JpaRepository<Plano, UUID> {
-}
