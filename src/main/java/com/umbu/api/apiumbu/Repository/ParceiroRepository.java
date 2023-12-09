e57968fe45ddea7c2f7711a31f49e201888af6c6
Lucas Bonfim <142261728+Lucas-Bonfim80@users.noreply.github.com>
2023-12-09 22:20:53 +0000

    Correção main

--- src/main/java/com/umbu/api/apiumbu/Repository/ParceiroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umbu.api.apiumbu.Repository;
-
-import com.umbu.api.apiumbu.Model.Parceiro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ParceiroRepository extends JpaRepository<Parceiro, UUID> {
-}
--- src/main/java/com/umbu/api/apiumbu/Repository/TrabalhoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umbu.api.apiumbu.Repository;
-
-import com.umbu.api.apiumbu.Model.Trabalho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TrabalhoRepository extends JpaRepository<Trabalho, UUID> {
-}
